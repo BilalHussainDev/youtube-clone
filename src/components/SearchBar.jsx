c3f091435d353f43a33a39f4e27abf883b1c930b
Bilal Hussain <bilal.hussain@magnusmage.com>
2024-06-05 09:56:56 +0500

    Add Searchbar Functionality

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,30 +1,44 @@
 import { IconButton, Paper } from "@mui/material";
 import SearchIcon from "@mui/icons-material/Search";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 
-function SearchBar() {
-	return (
-		<Paper
-			component="form"
-			onSubmit={() => {}}
-			sx={{
-				borderRadius: 20,
-				border: "1px solid #e3e3e3",
-				boxShadow: "none",
-				pl: 2,
-				mr: { sm: 5 },
-			}}
-		>
-			<input
-				className="search-bar"
-				placeholder="search"
-				value=""
-				onChange={() => {}}
-			/>
-			<IconButton sx={{ p: "10px", color: "red" }}>
-				<SearchIcon />
-			</IconButton>
-		</Paper>
-	);
-}
+const SearchBar = () => {
+  const [searchTerm, setSearchTerm] = useState("");
+  const navigate = useNavigate();
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (searchTerm) {
+      navigate(`/search/${searchTerm}`);
+      setSearchTerm("");
+    }
+  };
+
+  return (
+    <Paper
+      component="form"
+      onSubmit={handleSubmit}
+      sx={{
+        borderRadius: 20,
+        border: "1px solid #e3e3e3",
+        boxShadow: "none",
+        pl: 2,
+        mr: { sm: 5 },
+      }}
+    >
+      <input
+        className="search-bar"
+        placeholder="search"
+        value={searchTerm}
+        onChange={(e) => setSearchTerm(e.target.value)}
+      />
+      <IconButton sx={{ p: "10px", color: "red" }}>
+        <SearchIcon />
+      </IconButton>
+    </Paper>
+  );
+};
 
 export default SearchBar;
--- src/components/SearchFeed.jsx
+++ src/components/SearchFeed.jsx
@@ -1,5 +1,31 @@
+import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import { Box, Typography } from "@mui/material";
+import { Videos } from "src/components";
+import { fetchFromApi } from "src/utils/fetchFromApi";
+
 function SearchFeed() {
-	return <div>Search Feed Component</div>;
+  const [videos, setVideos] = useState([]);
+  const { searchTerm } = useParams();
+
+  useEffect(() => {
+    fetchFromApi(`/search/?query=${searchTerm}`).then((data) =>
+      setVideos(data.videos)
+    );
+  }, [searchTerm]);
+
+  return (
+    <Box p={2} sx={{ overflowY: "auto", flex: 2 }}>
+      <Typography variant="h4" fontWeight="bold" mb={2} sx={{ color: "white" }}>
+        Search Results for:{" "}
+        <Box component="span" sx={{ color: "#f31503" }}>
+          {searchTerm}
+        </Box>
+      </Typography>
+
+      <Videos videos={videos} />
+    </Box>
+  );
 }
 
 export default SearchFeed;
